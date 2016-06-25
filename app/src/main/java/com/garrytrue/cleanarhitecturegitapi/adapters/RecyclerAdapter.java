f8c75351f40c4152b3a0adbfb928fb60e77771de
Igor Torba <garrytrue@yandex.ru>
2016-06-25 14:16:15 +0300

    Add recycler adapter

---
+++ app/src/main/java/com/garrytrue/cleanarhitecturegitapi/adapters/RecyclerAdapter.java
@@ -0,0 +1,10 @@
+package com.garrytrue.cleanarhitecturegitapi.adapters;
+
+import java.util.List;
+
+/**
+ * Created by garrytrue on 25.06.16.
+ */
+public interface RecyclerAdapter<E> {
+    void setData(List<E> data);
+}
